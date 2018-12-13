7103f6dff7140d86e78b77e74ba7e2578e02bb01
Justin Chen <jchen06@stuy.edu>
2018-12-12 22:07:20 -0500

    Finished toString, added comments

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,7 +2,8 @@ public class MyLinkedList{
   private int size;
   private Node start,end;
 
-  public MyLinkedList() {
+  //constructor
+  public MyLinkedList() { //default MyLinkedList has nothing
     start = null;
     end = null;
     size = 0;
@@ -13,26 +14,31 @@ public class MyLinkedList{
   }
 
   public boolean add(int value) {
-    Node N = new Node(value);
+    Node N = new Node(value); //creates a new Node based off value
     size = size + 1;
-    if (size == 1) {
+    if (size == 1) {          //if there are no existing nodes
       start = N;
     }
-    if (size == 2) {
-      end = N;
-      start.setNext(end);
+    if (size == 2) {          //if there is a previous node
+      end = N;                //sets the end to the new Node
+      start.setNext(end);     //and has both nodes referencing each other
       end.setPrev(start);
     }
     if (size >= 3) {
-      end.setNext(N);
-      N.setPrev(end);
-      end = N;
+      end.setNext(N);         //last node references the new node being added
+      N.setPrev(end);         //new node being added references previous last node
+      end = N;                //now the end node is the new node
     }
     return true;
   }
 
   public String toString() {
     String ret = "";
-    
+    Node current = start;
+    for (int x = 0; x < size; x++) {
+      ret = ret + current.getData() + ", ";
+      current = current.getNext(); //swaps the current node to be the next node
+    }
+    return ret.substring(0,ret.length()-2); //gets rid of the extra ", "
   }
 }
