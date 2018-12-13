99bc0a1b498a29a45fc68c614f47f63f81c64632
Justin Chen <jchen06@stuy.edu>
2018-12-12 22:07:36 -0500

    Quick driver

---
+++ Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+  public static void main(String[] args) {
+    MyLinkedList A = new MyLinkedList();
+    A.add(10);
+    A.add(2);
+    A.add(5);
+    System.out.println(A);
+  }
+}
