0b57b7cf58aac98f5bcd68ba4b0f4babbecdd6b7
Justin Chen <jchen06@stuy.edu>
2018-12-16 21:23:26 -0500

    Finished comments, methods, testing

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -14,14 +14,14 @@ public class MyLinkedList{
   }
 
   public boolean add(int value) {
-    Node N = new Node(value); //creates a new Node based off value
+    Node N = new Node(value);     //creates a new Node based off value
     length = length + 1;
     if (length == 1) {         //if there are no existing nodes
       start = N;
     }
-    if (length == 2) {          //if there is a previous node
-      end = N;                //sets the end to the new Node
-      start.setNext(end);     //and has both nodes referencing each other
+    if (length == 2) {           //if there is a previous node
+      end = N;                   //sets the end to the new Node
+      start.setNext(end);        //and has both nodes referencing each other
       end.setPrev(start);
     }
     if (length >= 3) {
@@ -37,7 +37,7 @@ public class MyLinkedList{
     Node current = start;
     while (current != null) {
       ret = ret + current.getData() + ", ";
-      current = current.next(); //swaps the current node to be the next node
+      current = current.next();                   //swaps the current node to be the next node
     }
     if (ret.length() == 0) return "[]";
     return "[" + ret.substring(0,ret.length()-2) + "]"; //gets rid of the extra ", ", adds brackets to surround
@@ -48,13 +48,13 @@ public class MyLinkedList{
     if (index < 0 || index >= size()) throw new IndexOutOfBoundsException(); //exception thrown when out of bounds
     Node current = start;
     for (int x = 0; x < index; x++) { //from 0 to the index -1
-      current = current.next();       //the current node becomes the next (so gets the Nth node )
+      current = current.next();       //the current node becomes the next (so at index -1, gets the next, the Nth, node)
     }
     return current;
   }
 
   public Integer get(int index) {
-    return getNthNode(index).getData(); //returns the data integer
+    return getNthNode(index).getData(); //returns the data/integer
   }
 
   public Integer set(int index, Integer value) {
@@ -63,24 +63,24 @@ public class MyLinkedList{
     if (current == start) {
       current.next().setPrev(newNode);  //sets the previous node of the next to the newNode
       newNode.setNext(current.next());  //sets the next node of the newNode to the current Next node
-      start = newNode;                  //replaces start with newNode
+      start = newNode;                  //replaces start with newNode, and the old node disappears
     } else if (current == end) {
       current.prev().setNext(newNode);  //sets the next node of previous to the newNode
       newNode.setPrev(current.prev());  //sets the previous node of the newNode to the current previous Node
-      end = newNode;
-    } else {
-      current.next().setPrev(newNode);  //essentially combines the code above as the node is in the middle, not start/end
-      newNode.setNext(current.next());
-      current.prev().setNext(newNode);
-      newNode.setPrev(current.prev());
+      end = newNode;                    //replaces end with newNode, and the old node disappears
+    } else {                            //essentially combines the if blocks above
+      current.next().setPrev(newNode);  //sets the previous node of the next to the new
+      newNode.setNext(current.next());  //sets the next node of the newNode to the current next node
+      current.prev().setNext(newNode);  //sets the next node of the previous Node to the newNode
+      newNode.setPrev(current.prev());  //sets the previous node of the newNode to the current previous Node
     }
-    return get(index);
+    return get(index);                  //returns the integer/data that is replaced
   }
 
   public boolean contains(Integer value) {
     Node current = start;
     for (int x = 0; x < length; x++) {    //loops through the LinkedList
-      if (current.getData() == value) {
+      if (current.getData().equals(value)) {
         return true;                      //returns true if the value is equal to the data of current node
       }
       current = current.next();
@@ -90,45 +90,50 @@ public class MyLinkedList{
 
   public int indexOf(Integer value) {
     Node current = start;
-    for (int x = 0; x < length; x++) {
-      if (current.getData() == value) {
-        return x;
+    for (int x = 0; x < length; x++) {    //loops through the linked list
+      if (current.getData().equals(value)) {
+        return x;                         //stops loop and returns index if value is found
       }
       current = current.next();
     }
-    return -1;
+    return -1;                            //otherwise returns -1 as value is not found
   }
 
   public void add(int index, Integer value) {
-      Node current = getNthNode(index);
-      Node newNode = new Node(value);
-      if (current == start) {
-        current.setPrev(newNode);
-        newNode.setNext(current);
-        start = newNode;
-      } else if (index == length) {
-        add(index);
+      Node current, newNode, temp;
+      newNode = new Node(value);                    //creates a new Node based off value
+      if (index == length) {                        //if the index is the length of the LinkedList, essentially adds to the end
+        end.setNext(newNode);                       //swaps and sets newNode to be the end node
+        newNode.setPrev(end);
+        end = newNode;
+      } else {
+        current = getNthNode(index);                //else gets the Nth node and sets it to be the current
+        if (current == start) {                     //if the current Node is the start
+          current.setPrev(newNode);                 //sets the previous node of the start node to the newNode
+          newNode.setNext(current);                 //sets next node of the newNode to the current start node
+          start = newNode;                          //the start node is now the newNode
+        } else {
+          temp = current.prev();                     //sets the temporary node the previous node of the current
+          temp.setNext(newNode);                     //sets the next node of the temp to the new Node
+          newNode.setPrev(temp);                     //sets previous node of newNode to temp
+          newNode.setNext(current);                  //sets next node of newNode to current
+          current.setPrev(newNode);                  //sets previous node of current to NewNode
+        }
       }
-      else {
-        current.prev().setNext(newNode);
-        newNode.setPrev(current.prev());
-        current.setPrev(newNode);
-        newNode.setNext(current);
-      }
-      length++;
-  }
+      length++;     //adds length +1
+}
 
   public Integer remove(int index) {
     Node current = getNthNode(index);
     if (current == start) {
-      current.next().setPrev(null);
-      start = current.next();
+      current.next().setPrev(null);               //if current is start, changes next node to have a null previous node
+      start = current.next();                     //makes the next node the start
     } else if (current == end) {
-      current.prev().setNext(null);
-      end = current.prev();
+      current.prev().setNext(null);               //if current is end, changes the previous node to have a null next node
+      end = current.prev();                       //makes the previous node the end
     }
     else {
-      current.prev().setNext(current.next());
+      current.prev().setNext(current.next());     //otherwise just changes references for past and previous node to reference each other
       current.next().setPrev(current.prev());
     }
     length--;
@@ -136,11 +141,12 @@ public class MyLinkedList{
   }
 
   public boolean remove(Integer value) {
-    if (contains(value)) {
-      int index = indexOf(value);
-      remove(index);
-      return true;
+    if (contains(value)) {            //checks if value is present
+      int I = indexOf(value);
+      System.out.println(I);
+      remove(I);
+      return true;                   //finds the index, removes that index, and returns true
     }
-    return false;
+    return false;                   //false is returned otherwise
   }
 }
